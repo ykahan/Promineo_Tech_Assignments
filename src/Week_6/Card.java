134868e497098aa769474c387b69e6be0ade65a3
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2020-06-14 01:56:03 +0300

    Card class

---
+++ src/Week_6/Card.java
@@ -0,0 +1,32 @@
+package Week_6;
+
+public class Card {
+    private int value;
+    private String name;
+
+    public Card(int value, String name){
+        this.value = value;
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
+}

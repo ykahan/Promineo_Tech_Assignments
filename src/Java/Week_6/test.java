7b4275bf64e13d68bccac1259a8eaf0855bc1ad2
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2020-07-09 14:02:03 +0300

    Java week 6

--- src/Java/Week_6/test.java
+++ src/Java/Week_6/test.java
@@ -1,7 +1,10 @@
 package Java.Week_6;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class test {
     public static void main(String[] args) {
-        Deck deck = new Deck();
+
     }
 }

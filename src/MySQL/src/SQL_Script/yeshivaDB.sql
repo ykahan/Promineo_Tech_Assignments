91c8b5361047f3e61768e994ec919a87e42a7083
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2020-09-08 15:32:57 +0300

    Added DROP TABLE IF NOT EXISTS lines

--- src/MySQL/src/SQL_Script/yeshivaDB.sql
+++ src/MySQL/src/SQL_Script/yeshivaDB.sql
@@ -2,10 +2,12 @@ DROP DATABASE IF EXISTS talmudic_studies;
 CREATE DATABASE talmudic_studies;
 USE talmudic_studies;
 
+DROP TABLE IF EXISTS tractates;
 CREATE TABLE IF NOT EXISTS tractates(
     id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
-    pages INT(10) NOT NULL
+    maxPages INT(10) NOT NULL,
+    maxDaf VARCHAR(20) NOT NULL
 );
 
 INSERT INTO tractates(name, pages) VALUES("Brachos", 125);
@@ -21,13 +23,14 @@ CREATE TABLE IF NOT EXISTS students(
 INSERT INTO students(personal_name, family_name) VALUES("Yehoshua", "Kahan");
 INSERT INTO students(personal_name, family_name) VALUES("Ploni", "Almoni");
 
+DROP TABLE IF EXISTS students_tractates;
 CREATE TABLE IF NOT EXISTS students_tractates(
     id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     student_id INT(10) NOT NULL,
     FOREIGN KEY (student_id) REFERENCES students(id),
     tractate_id INT(10) NOT NULL,
     FOREIGN KEY (tractate_id) REFERENCES tractates(id),
-    page INT(10) NOT NULL,
+    daf VARCHAR(20) NOT NULL,
     date_studied TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
 

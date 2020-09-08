d1b1293ea8283e276fb65d41aeb742236965988f
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2020-09-09 00:55:47 +0300

    Split switch code into individual methods, created getDate() method

--- src/MySQL/src/Application/Application.java
+++ src/MySQL/src/Application/Application.java
@@ -6,11 +6,12 @@ import Database_Controls.tractates_dao;
 
 import java.sql.Date;
 import java.util.Arrays;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Scanner;
 
 public class Application {
-    Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         boolean continueApp = true;
@@ -18,90 +19,151 @@ public class Application {
             printMenu();
             int choice = getUserChoice();
             switch (choice) {
-                case (1): // display all tractates
+                case (1):
                     tractates_dao.displayAllTracs();
                     break;
-                case (2): // display one tractate
-                    String tracName = tractates_dao.getTracName();
-                    boolean tracFound = tractates_dao.doesTracExist(tracName);
-                    if(tracFound) tractates_dao.displayOneTrac(tracName);
-                    else Messages.tracNotFound();
+                case (2):
+                    displayOneTrac();
+                    String trac;
                     break;
-                case (3): // display all student names
+                case (3):
                     students_dao.displayAllStudentNames();
                     break;
-                case (4): // add new tractate to db
-                    tracName = tractates_dao.getTracName();
-                    int tracPages = tractates_dao.getTracPages();
-                    tractates_dao.addTracToDB(tracName, tracPages);
+                case (4):
+                    addNewTracToDB();
                     break;
-                case (5): // add new student
-                    String studentPersonalName = students_dao.getStudentPersonalName();
-                    String studentFamilyName = students_dao.getStudentFamilyName();
-                    students_dao.addStudentToDB(studentPersonalName, studentFamilyName);
+                case (5):
+                    addNewStudentToDB();
                     break;
-                case (6): // edit student name
-                    String oldStudentPersonalName = students_dao.getStudentPersonalName();
-                    String oldStudentFamilyName = students_dao.getStudentFamilyName();
-                    boolean studentFound = students_dao.doesStudentExist(oldStudentPersonalName, oldStudentFamilyName);
-                    if (studentFound) {
-                        Messages.needNewStudentName();
-                        String newStudentPersonalName = students_dao.getStudentPersonalName();
-                        String newStudentFamilyName = students_dao.getStudentFamilyName();
-                        students_dao.changeStudentName(
-                                oldStudentPersonalName,
-                                oldStudentFamilyName,
-                                newStudentPersonalName,
-                                newStudentFamilyName
-                        );
-                    } else {
-                        Messages.studentNotFound();
-                    }
+                case (6):
+                    editStudentName();
                     break;
                 case(7): // delete student from DB
-                    studentPersonalName = getStudentPersonalName();
-
-                    studentFamilyName = getStudentFamilyName();
-                    studentFound = students_dao.doesStudentExist(studentPersonalName, studentFamilyName);
-                    if(studentFound) students_dao.deleteStudent(studentPersonalName, studentFamilyName);
-                    else {
-                        Messages.studentNotFound();
-                    }
+                    deleteStudentFromDB();
                     break;
                 case(8): // record student learned daf
-                    studentPersonalName = students_dao.getStudentPersonalName();
-                    studentFamilyName = students_dao.getStudentFamilyName();
-                    studentFound = students_dao.doesStudentExist(studentPersonalName, studentFamilyName);
+                    String stuPerName = students_dao.getStudentPersonalName();
+                    String stuFamName = students_dao.getStudentFamilyName();
+                    boolean studentFound = students_dao.doesStudentExist(stuPerName, stuFamName);
                     if(studentFound) {
-                        tracName = tractates_dao.getTracName();
-                        int daf = tractates_dao.getDaf();
-                        char amud = tractates_dao.getAmud();
-                        int page = tractates_dao.dafToPage(daf, amud);
+                        trac = tractates_dao.getTracName();
+                        int page = getPage();
                         boolean pageIsValid = tractates_dao.isPageValid(page);
                         if(pageIsValid) {
-                            boolean userProvidedDate = students_tractates_dao.useUserProvidedDate();
-                            if(userProvidedDate) {
-                                Date date = students_tractates_dao.getDate();
-                                students_tractates_dao.enterNewRow(
-                                       studentPersonalName,
-                                       studentFamilyName,
-                                       page,
-                                       date
-                                );
-                            } else {
-                                students_tractates_dao.enterNewRow(
-                                        studentPersonalName,
-                                        studentFamilyName,
-                                        page
-                                );
+                            boolean getDate = students_tractates_dao.useUserProvidedDate();
+                            recordLearning(
+                                    getDate,
+                                    stuPerName,
+                                    stuFamName,
+                                    trac,
+                                    page);
+
                             }
                         }
-                    }
+                    break;
                 case(9): // break out of app
                     continueApp = false;
                     Messages.goodbye();
             }
         }
+    }
+
+    private static void recordLearning(boolean getDate, String stuPerName, String stuFamName, String trac, int page) {
+        if(getDate){
+            Date date = getDate();
+            student_tractates_dao.enterNewRow(stuPerName, stuFamName, trac, page, date);
+        } else{
+            student_tractates_dao.enterNewRow(stuPerName, stuFamName, trac, page);
+        }
+    }
+
+    private static Date getDate() {
+        int year = getYear();
+        int month = getMonth();
+        int day = getDay();
+        return (Date) new GregorianCalendar(year, month - 1, day).getTime();
+    }
+
+    private static int getDay() {
+        Messages.getDay();
+        String dayStr = scanner.nextLine();
+        return Integer.parseInt(dayStr);
+    }
+
+    private static int getMonth() {
+        Messages.getMonth();
+        String monthStr = scanner.nextLine();
+        return Integer.parseInt(monthStr);
+    }
+
+    private static int getYear() {
+        Messages.getYear();
+        String yearStr = scanner.nextLine();
+        int year = Integer.parseInt(yearStr);
+        return year;
+    }
+
+    private static long millisSinceEpoch(String year, String month, String day) {
+        int yearsPassed = Integer.parseInt(year) - 1970;
+        int monthsPassed = Integer.parseInt(month) - 1 + (yearsPassed * 12);
+        int daysPassed = Integer.parseInt(day) - 1;
+    }
+
+    private static int getPage() {
+        int daf = tractates_dao.getDaf();
+        char amud = tractates_dao.getAmud();
+        int page = tractates_dao.dafToPage(daf, amud);
+        return page;
+    }
+
+    private static void deleteStudentFromDB() {
+        String studentPersonalName = getStudentPersonalName();
+        String studentFamilyName = getStudentFamilyName();
+        boolean studentFound = students_dao.doesStudentExist(studentPersonalName, studentFamilyName);
+        if(studentFound) students_dao.deleteStudent(studentPersonalName, studentFamilyName);
+        else {
+            Messages.studentNotFound();
+        }
+        return;
+    }
+
+    private static void editStudentName() {
+        String oldStudentPersonalName = students_dao.getStudentPersonalName();
+        String oldStudentFamilyName = students_dao.getStudentFamilyName();
+        boolean studentFound = students_dao.doesStudentExist(oldStudentPersonalName, oldStudentFamilyName);
+        if (studentFound) {
+            Messages.needNewStudentName();
+            String newStudentPersonalName = students_dao.getStudentPersonalName();
+            String newStudentFamilyName = students_dao.getStudentFamilyName();
+            students_dao.changeStudentName(
+                    oldStudentPersonalName,
+                    oldStudentFamilyName,
+                    newStudentPersonalName,
+                    newStudentFamilyName
+            );
+        } else {
+            Messages.studentNotFound();
+        }
+    }
+
+    private static void addNewStudentToDB() {
+        String studentPersonalName = students_dao.getStudentPersonalName();
+        String studentFamilyName = students_dao.getStudentFamilyName();
+        students_dao.addStudentToDB(studentPersonalName, studentFamilyName);
+    }
+
+    private static void addNewTracToDB() {
+        String tracName;
+        tracName = tractates_dao.getTracName();
+        int tracPages = tractates_dao.getTracPages();
+        tractates_dao.addTracToDB(tracName, tracPages);
+    }
+
+    private static void displayOneTrac() {
+        String tracName = tractates_dao.getTracName();
+        boolean tracFound = tractates_dao.doesTracExist(tracName);
+        if(tracFound) tractates_dao.displayOneTrac(tracName);
+        else Messages.tracNotFound();
     }
 
     public static void printMenu() {
@@ -126,5 +188,4 @@ public class Application {
         scanner.nextLine();
         return choice;
     }
-
 }

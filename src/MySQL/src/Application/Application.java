34dc037f64213c4878deb66af9d44c403bfbb031
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2020-09-08 15:10:44 +0300

    Completed application psvm

--- src/MySQL/src/Application/Application.java
+++ src/MySQL/src/Application/Application.java
@@ -1,4 +1,14 @@
 package Application;
 
 public class Application {
+    public static void main(String[] args) {
+        boolean continueApp = true;
+        while(continueApp){
+            printMenu();
+        }
+    }
+
+    public static void printMenu(){
+
+    }
 }
